1e1a5018eef3e274f9bad64145e0b6bfdecd6362
Ochuodho <ochuodhoderrick@gmail.com>
2023-10-25 14:41:32 +0300

    need to add models

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
+from flask_restful import Resource, Api
 
-from models import db, Hero
+from models import db, Hero, HeroPower,Power
 
 app = Flask(__name__)
+api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = True
 
 migrate = Migrate(app, db)
 
@@ -15,7 +18,21 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return ''
+    return 'Superheroes API'
+
+
+class Heroes(Resource):
+    def get(self):
+        heroes = []
+        for hero in Hero.query.all():
+            hero_dict = {
+                "id": hero.id,
+                "name": hero.name,
+                "super_name": hero.super_name
+            }
+            heroes.append(hero_dict)
+
+        return make_response(jsonify(heroes), 200)
 
 
 if __name__ == '__main__':
